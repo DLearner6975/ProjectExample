db3a4290954b126cbeec7497143db2982ac804d4
DLearner6975 <outtacontrol6975@gmail.com>
2017-04-25 20:57:12 -0500

    Adding models and Ef migrations to the projects

--- Vegas/Models/Make.cs
+++ Vegas/Models/Make.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vegas.Models
 {
@@ -11,6 +12,9 @@ namespace Vegas.Models
 
         }
         public int Id { get; set; }
+
+        [Required]
+        [StringLengthAttribute(255)]
         public string Name { get; set; }
         public ICollection<Model> Models { get; set; }
     }
