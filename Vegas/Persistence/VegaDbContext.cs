7355dd648a86adf23676f7161f71c5c19b58a7c0
DLearner6975 <outtacontrol6975@gmail.com>
2017-04-25 22:14:17 -0500

    Correct model name in feature

--- Vegas/Persistence/VegaDbContext.cs
+++ Vegas/Persistence/VegaDbContext.cs
@@ -11,6 +11,6 @@ namespace Vegas.Persistence
         }
 
         public DbSet<Make> Makes { get; set; }
-        public DbSet<Features> Features { get; set; }
+        public DbSet<Feature> Features { get; set; }
     }
 }
